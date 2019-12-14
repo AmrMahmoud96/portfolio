86395093a6461cfa1b331371a26bef564233100d
amrmahmoud <amrmahmoud.am@hotmail.com>
2019-12-14 17:47:39 -0500

    Updates

---
+++ precache-manifest.4a455620e0ee54318afe24eb88459ca8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a7921375211adcc9627cede5a4ef6d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c0ee8428e6e23a638ef0",
+    "url": "/portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c5588012c9f61586532d",
+    "url": "/portfolio/static/css/main.0a678f2f.chunk.css"
+  },
+  {
+    "revision": "c0ee8428e6e23a638ef0",
+    "url": "/portfolio/static/js/2.95666481.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/portfolio/static/js/2.95666481.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5588012c9f61586532d",
+    "url": "/portfolio/static/js/main.18c41828.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "cace8b7bfadc48e7e9dcd79725111705",
+    "url": "/portfolio/static/media/Headshot.cace8b7b.png"
+  },
+  {
+    "revision": "2afaf81ac14b94f76f48411a05673e99",
+    "url": "/portfolio/static/media/connectwork.2afaf81a.png"
+  }
+]);
