e6b889245d8b19936b80b4eebc283199a04d6d2d
amrmahmoud <amrmahmoud.am@hotmail.com>
2019-12-28 20:05:46 -0500

    Updates

---
+++ precache-manifest.a64567f8c1e535ffd9e9446d715732a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9b2afa3a6e611a0045d5185e05ef25",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f44e208945100d53f5f7",
+    "url": "/portfolio/static/css/2.29546f59.chunk.css"
+  },
+  {
+    "revision": "bc279525b69bbf66f34b",
+    "url": "/portfolio/static/css/main.41ad959c.chunk.css"
+  },
+  {
+    "revision": "f44e208945100d53f5f7",
+    "url": "/portfolio/static/js/2.71cfbf68.chunk.js"
+  },
+  {
+    "revision": "36587352e7815de0ba225b441f686430",
+    "url": "/portfolio/static/js/2.71cfbf68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc279525b69bbf66f34b",
+    "url": "/portfolio/static/js/main.06b3db5f.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "374ec0b3abbf296073491f7f850da2f3",
+    "url": "/portfolio/static/media/Headshot.374ec0b3.jpg"
+  },
+  {
+    "revision": "2afaf81ac14b94f76f48411a05673e99",
+    "url": "/portfolio/static/media/connectwork.2afaf81a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c04bbb7a44d6564a2022faa7b36d5db0.js"
+  "/portfolio/precache-manifest.a64567f8c1e535ffd9e9446d715732a7.js"
 );
 
 self.addEventListener('message', (event) => {
