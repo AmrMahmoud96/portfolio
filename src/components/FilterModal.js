67ff4807a98fedb428bc2d38bd8cbdd3a9c1a8f0
amrmahmoud <amrmahmoud.am@hotmail.com>
2020-01-09 15:03:00 -0500

    filter shortcut

---
+++ src/components/FilterModal.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './FilterModal.css';
+import Modal from 'react-modal';
+
+import {Button}  from 'react-bootstrap';
+
+export default class FilterModal extends Component {
+  constructor(props){
+    super(props)
+    Modal.defaultStyles.overlay.backgroundColor = '#232121';
+  }
+
+  render() {
+
+    const style={
+      content:{
+        display:'flex',
+        flexFlow:'column',
+        padding: '2rem'
+      }
+    }
+
+    return(
+      <div>
+      <Modal
+      ariaHideApp={false}
+      style={style}
+          isOpen={this.props.show}
+          onRequestClose={this.props.close}
+        >
+        <div className="modal-header">
+          <div>
+            <h3>Filter Menu</h3>
+          </div>
+          <div >
+            <h1 onClick={this.props.close} className='close-modal-btn'>X</h1>
+          </div>
+        </div>
+
+          <div className="full-filter-container">
+            <h4 className="filter-title">Currently displaying content for the following:</h4>
+            <div className="centerflex">
+            <div className="filter-container">
+              {this.props.tags}
+            </div>
+            </div>
+          </div>
+
+        <div style={{'textAlign':'center','margin-top':'auto'}}>
+        <h4  style={{'margin-bottom':'5px'}}>Click icons to remove filters</h4>
+        <p>or</p>
+        <Button onClick={this.props.clearAll}  variant="danger" size='lg'>Clear All</Button>
+
+        </div>
+
+        </Modal>
+      </div>
+    );
+
+    // let carouselItems=<div><img src="images/UofTHacks/interac.jpeg"/></div>
+
+    // if(this.props.data.images){
+    //   // const images = this.importAll(require.context('../../public/images/UofTHacks', false, /\.(png|jpe?g|svg)$/));
+    //   // console.log(images)
+    //   // if (images){
+    //
+    //    const carouselItems = this.props.data.images.forEach((i, index) => {
+    //      console.log(i)
+    //      // console.log('images/'+this.props.data.images+'/'+i)
+    //       return (
+    //             <div key={index}>
+    //                 <img src = {i}/>
+    //                 <p>{i}</p>
+    //              </div>
+    //            )
+    //       // return <Icon key={index} onClick={this.props.filterAction} data={skill} dimensions={d}/>
+    //     });
+    //     return (
+    //
+    //       <div>
+    //       <Modal
+    //       style={style}
+    //           isOpen={this.props.show}
+    //           onRequestClose={this.props.close}
+    //         >
+    //         <h1>{this.props.data.title}</h1>
+    //         <p>this is some test2</p>
+    //         <Carousel useKeyboardArrows>
+    //                 {carouselItems}
+    //
+    //         </Carousel>
+    //         </Modal>
+    //       </div>
+    //     );
+    //   //}
+    // }
+    //
+    // else{
+    //   return (
+    //
+    //     <div>
+    //     <Modal
+    //     style={style}
+    //         isOpen={this.props.show}
+    //         onRequestClose={this.props.close}
+    //       >
+    //       <h1>{this.props.data.title}</h1>
+    //       <p>this is some test</p>
+    //       </Modal>
+    //     </div>
+    //   );
+    // }
+  }
+}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,6 +3,7 @@ import './Skills.css';
 import Icon from './SkillsIcon'
 import TagIcon from './TagIcon'
 import {Button}  from 'react-bootstrap';
+import FilterModal from './FilterModal'
 
 const skills = require('../resources/skills.json')
 
@@ -10,24 +11,38 @@ export default class Skills extends Component {
 
   constructor(props) {
    super(props);
-   this.state = {isToggleOn: true,selected:"none"};
-// this.props.onClick = this.props.onClick.bind(this)
-   // This binding is necessary to make `this` work in the callback
-   // this.filterContent = this.filterContent.bind(this);
+   this.state = {isToggleOn: true,selected:"none",showModal:false};
+
    this.matchSkill=this.matchSkill.bind(this);
+   this.handleOpenModal = this.handleOpenModal.bind(this);
+   this.handleCloseModal = this.handleCloseModal.bind(this);
 
- }
+   }
+   handleOpenModal () {
+   this.lockBodyScrolling()
+   this.setState({ showModal: true });
+   }
 
-//  filterContent(e,name,callback) {
-// // window.filter(name);
-//
-//    this.setState(state => ({
-//      isToggleOn: !state.isToggleOn,
-//      selected:name
-//    }));
-//    callback()
-//    e.preventDefault()
-//  }
+   handleCloseModal () {
+   this.restoreBodyScrolling()
+   this.setState({ showModal: false });
+   }
+
+   lockBodyScrolling() {
+   // Get scroll position to be able to restore it in onCloseModal
+   this.bodyScrollPos = document.body.scrollTop
+     || document.documentElement.scrollTop || 0;
+   this.appWrapper.style.position = 'fixed';
+   this.appWrapper.style.top = `-${this.bodyScrollPos}px`;
+   }
+   restoreBodyScrolling() {
+   this.appWrapper.style.removeProperty('position');
+   this.appWrapper.style.removeProperty('top');
+   document.documentElement.scrollTop = document.body.scrollTop = this.bodyScrollPos;
+   }
+   componentDidMount() {
+   this.appWrapper = document.getElementById('root');
+   }
     matchSkill(s){
       var choices = skills.filter(function(skill){
         return skill.text == s;
@@ -36,7 +51,9 @@ export default class Skills extends Component {
       else{return null}
     }
   render() {
-
+    if(this.state.showModal && !this.props.filtering){
+      this.handleCloseModal()
+    }
     const d={
       width:'100px',
       height:'100px'
@@ -52,17 +69,12 @@ export default class Skills extends Component {
         <h1 className="section-title" style={{backgroundColor:'black'}}>Skills</h1>
         <h2 className="skills-intro">Select any combination of skills to filter the contents of the page.</h2>
         <div className="skills-container">{icons}</div>
-        {this.props.filtering &&
-          <div className="full-filter-container">
-            <h4>Currently displaying content for the following:</h4>
-            <div className="centerflex">
-            <div className="filter-container">
-              {tags}
-            </div>
-            </div>
-            <Button className="clear-button"onClick={this.props.clearAll}  variant="primary" size='lg'>Clear</Button>
-          </div>
-        }
+
+        <div className={this.props.filtering? "filter-shortcut":"unseen"} onClick={this.handleOpenModal} >
+          <img src="images/filter.png"/>
+        </div>
+        <FilterModal tags={tags} filtering={this.props.filtering} clearAll={this.props.clearAll} show={this.state.showModal && this.props.filtering} close={this.handleCloseModal}/>
+
 
 
 
